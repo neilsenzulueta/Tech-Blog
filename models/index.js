153159924aad79b2bbf981a021a2e55ea356efd2
neilsen zulueta <neilzulueta@gmail.com>
2024-01-04 23:41:30 -0800

    update comment and index.js

--- models/index.js
+++ models/index.js
@@ -1,3 +1,29 @@
 const User = require('./User');
+const Post = require('./post');
+const Comment = require('./comment');
 
-module.exports = { User };
+User.hasMany(Post, {
+    foreignKey: 'user_id',
+});
+
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+});
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id',
+});
+
+User.hasMany(Comment, {
+    foreignKey: 'user_id',
+});
+
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id,'
+});
+
+Post.hasMany(Comment, {
+    foreignKey: 'post_id',
+});
+
+module.exports = { User, Post, Comment };
